687fd678d3a084275b45d7c615678913e36332fc
Petro <worldinthegame@gmail.com>
2025-02-22 12:41:31 +0200

    fix bug

--- main.js
+++ main.js
@@ -1,55 +1,51 @@
 //Open burger-menu
+document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('click', function (e) {
+    documentClick(e); // Викликає першу функцію
+  });
 
-document.addEventListener('click', function (e) {
-  documentClick(e); // Викликає першу функцію
+  let isMoved = false; // Стежимо за станом елемента (переміщений чи ні)
+  const originalContainer = document.querySelector('.header__conteiner'); // Початковий контейнер
+
+  function documentClick(e) {
+    const targetItem = e.target;
+    
+    if (targetItem.closest('.burger-menu__icon')) {
+      document.documentElement.classList.toggle('open');
+      document.body.classList.toggle('_lock');  // Заберає прокрутку сторінки
+    }
+  }
+  // Прокрутка при кліку nav
+
+  const menuLinks = document.querySelectorAll('.nav__link[data-goto]');
+  console.log("e", menuLinks)
+  if(menuLinks.length > 0) {
+    console.log("1")
+    menuLinks.forEach(menuLinks => {
+      menuLinks.addEventListener('click', onMenuLinckClick);
+      console.log("2")
+    })
+
+    function onMenuLinckClick(e) {
+      const menuLink = e.target;
+      console.log("3")
+      if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+        console.log("f")
+        const gotoBlock = document.querySelector(menuLink.dataset.goto);
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+        
+        // Закриваємо меню після скролінгу
+        document.documentElement.classList.remove('open');
+        document.body.classList.remove('_lock');  // Відновлює прокрутку
+
+        window.scrollTo({
+          top: gotoBlockValue,
+          behavior: 'smooth'
+        });
+
+
+        e.preventDefault();
+      }
+    }
+  }
 });
-
-let isMoved = false; // Стежимо за станом елемента (переміщений чи ні)
-const originalContainer = document.querySelector('.header__conteiner'); // Початковий контейнер
-
-function documentClick(e) {
-  const targetItem = e.target;
-
-  if (targetItem.closest('.burger-menu__icon')) {
-    document.documentElement.classList.toggle('open');
-    document.body.classList.toggle('_lock');  // Заберає прокрутку сторінки
-    moveElement();  // Викликаємо функцію переміщення елемента
-  }
-}
-
-function moveElement() {
-  const container = document.querySelector('.nav__body');
-  const item1 = document.querySelector('.comunication');
-
-  if (container && item1 && originalContainer) {
-    if (!isMoved) {
-      container.appendChild(item1); // Переміщуємо елемент у кінець нового контейнера
-    } else {
-      originalContainer.appendChild(item1); // Повертаємо елемент у початковий контейнер
-    }
-    isMoved = !isMoved; // Змінюємо стан
-  }
-}
-
-// Прокрутка при кліку nav
-
-const menuLinks = document.querySelectorAll('.nav_link[data-goto]');
-if(menuLinks.length > 0) {
-  menuLinks.forEach(menuLinks => {
-    menuLinks.addEventListener('click', onMenuLinckClick);
-  })
-
-  function onMenuLinckClick(e) {
-    const menuLink = e.target;
-    if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-      const gotoBlock = document.querySelector(menuLink.dataset.goto);
-      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYoffset - document.querySelector('header').offsetHeight;
-
-      window.scrollTo({
-        top: gotoBlockValue,
-        behavior: 'smooth'
-      });
-      e.prefentDefault();
-    }
-  }
-}
